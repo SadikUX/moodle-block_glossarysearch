ddd5ff8f2f1044836b38885ba77c600c9c85cd3b
Classic-Al <alanb.chadwick@gmail.com>
2025-09-13 11:20:33 +0100

    Bump plugin version to 0.4.0 (version.php)

--- version.php
+++ version.php
@@ -6,4 +6,4 @@ $plugin->component = 'block_glossarysearch'; // Full frankenstyle name.
 $plugin->version   = 2025082800;             // YYYYMMDDXX build stamp.
 $plugin->requires  = 2024042200;             // Requires Moodle 4.5 (2024042200).
 $plugin->maturity  = MATURITY_BETA;         // Development stage: ALPHA, BETA, RC, or STABLE.
-$plugin->release   = '0.3.0';                // Human-friendly version.
+$plugin->release   = '0.4.0';                // Human-friendly version.
