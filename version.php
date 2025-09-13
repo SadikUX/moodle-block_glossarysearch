ffd7afe94e4db811a989cef1ea8a69d8035be140
Sadik Mert <sadik@Mac.fritz.box>
2025-09-13 23:05:07 +0200

    Version update

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_glossarysearch';
-$plugin->version   = 2025082800;
+$plugin->version   = 2025091302;
 $plugin->requires  = 2024042200;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.4.0';
+$plugin->release   = '1.0.0';
